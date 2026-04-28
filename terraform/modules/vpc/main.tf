da4cc580e9f9a5e2180d37e43055742c1918ee2b
jinsw1 <jin_sw1@naver.com>
2026-04-28 18:11:28 +0900

    terraform 구조화-작업중01

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,52 @@
+# vpc
+resource "aws_vpc" "proejct01_vpc" {
+	cidr_block 				= "10.0.0.0/16"
+	enable_dns_hostnames 	= true
+	tags 					= { Name = "project01-vpc" }
+}
+
+# igw (인터넷 게이트웨이)
+resource "aws_internet_gateway" "igw" {
+  # 위에서 만들어진 vpc 의 아이디를 참조 하도록 한다.connection {
+  vpc_id 	= aws_vpc.proejct01_vpc.id 
+  tags  = {
+	Name = "project01-igw" 
+  }
+}
+
+#현재 리전에서 사용가능한(avaliable) 가용영역 데이터 가져오기
+data "aws_availability_zones" "available"{
+	state = "available"
+}
+
+# public subnet
+resource "aws_subnet" "public_subnet_bastion" {
+  vpc_id 					= aws_vpc.proejct01_vpc.id
+  cidr_block 				= "10.0.1.0/24" # 256개의 ip 할당
+
+  # data.aws_availability_zones.available.names 은 배열인데 거기에 여러개의 가용영역 데이터가 들어있다.
+  # 그중 0번 방에 있는 데이터를 연결한다.
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  map_public_ip_on_launch 	= true # 이 방에 생기는 서버는 무조건 공인 ip를 받는다. false면 private_subnet
+  tags = {
+	Name = "lecture-subnet" 
+  }
+}
+
+# private subnet was
+resource "aws_subnet" "private_subnet_was" {
+  vpc_id 					= aws_vpc.proejct01_vpc.id
+  cidr_block 				= "10.0.10.0/24" # 256개의 ip 할당
+
+  # data.aws_availability_zones.available.names 은 배열인데 거기에 여러개의 가용영역 데이터가 들어있다.
+  # 그중 0번 방에 있는 데이터를 연결한다.
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  map_public_ip_on_launch 	= false # 이 방에 생기는 서버는 무조건 공인 ip를 받는다. false면 private_subnet
+  tags = {
+	Name = "project01-private-subnet-was"
+  }
+}
+
+
