6c5c47d921056335ddb8c526811ee51eb2344054
jinsw1 <jin_sw1@naver.com>
2026-04-29 16:10:29 +0900

    terraform 기본 템플릿 오류 수정

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -4,21 +4,21 @@ data "aws_availability_zones" "available" {
 
 #vpc 및 네트워크 생성
 module "vpc" {
-  source     = "./modules/vpc"
+  source     = "../../modules/vpc"
   cidr_block = "10.0.0.0/16"
   name       = "project01-vpc"
 }
 
 #igw 인터넷 게이트웨이 생성
 module "igw" {
-  source  = "./modules/internet_gateway"
+  source  = "../../modules/internet-gateway"
   vpc_id  = module.vpc.vpc_id
   name    = "project01-igw"
 }
 
 # public 서브넷 생성
 module "public_subnet_bastion" {
-  source = "./modules/subnet"
+  source = "../../modules/subnet"
 
   vpc_id            = module.vpc.vpc_id
   cidr_block        = "10.0.1.0/24"
@@ -30,7 +30,7 @@ module "public_subnet_bastion" {
 
 # private was 서브넷 생성
 module "private_subnet_was" {
-  source = "./modules/subnet"
+  source = "../../modules/subnet"
 
   vpc_id            = module.vpc.vpc_id
   cidr_block        = "10.0.10.0/24"
@@ -40,12 +40,14 @@ module "private_subnet_was" {
   name              = "project01-private-subnet-was"
 }
 
+/*
 # private db 서브넷 생성
 module "private_subnet_db" {
-	source = "./modules/subnet"
+	source = "../../modules/subnet"
 }
 
 # private db 서브넷 생성
 module "private_subnet_alb" {
-	source = "./modules/subnet"	
+	source = "../../modules/subnet"	
 }
+*/
