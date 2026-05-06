e9e68743305653f0885f5e8ae2344aa0abe79efc
gyu <leesk000107@naver.com>
2026-05-06 17:37:49 +0900

    feat: mgmt sg, ec2, key-pair 생성

--- terraform/envs/dev/base/main.tf
+++ terraform/envs/dev/base/main.tf
@@ -41,10 +41,48 @@ module "private_subnet_was" {
   subnet_name             = "project01-private-subnet-was"
 }
 
-# # private db 서브넷 생성
-# module "private_subnet_db" {
-# 	source = "./modules/subnet"
-# }
+# private db 서브넷 생성
+module "private_subnet_db" {
+  source = "../../../modules/subnet"
+
+  vpc_id           = module.vpc.vpc_id
+  cidr_block       = "10.0.20.0/24"
+  az               = data.aws_availability_zones.available.names[0]
+
+  map_public_ip    = false
+  subnet_name             = "project01-private-subnet-db"
+}
+
+# 키 페어 설정
+module "key_pair" {
+  source   = "../../../modules/key"
+  
+  key_name = "project01-key" # 생성될 pem 파일의 이름
+}
+
+# mgmt용 보안 그룹 설정
+module "sg_mgmt" {
+  source        = "../../../modules/security-group"
+  
+  vpc_id        = module.vpc.vpc_id
+  sg_name       = "project01-mgmt-sg"
+  ingress_ports = [22]
+}
+
+# EC2_MGMT 생성
+module "ec2_mgmt" {
+  source        = "../../../modules/ec2"
+  
+  name          = "project01-mgmt-ec2"
+  instance_type = "t3.micro"
+  
+  # 퍼블릭 서브넷에 배치
+  subnet_id     = module.public_subnet_bastion.subnet_id 
+  
+  # 보안 그룹과 키 페어를 연결
+  sg_ids        = [module.sg_mgmt.sg_id] 
+  key_name      = module.key_pair.key_name
+}
 
 # # private db 서브넷 생성
 # module "private_subnet_alb" {
