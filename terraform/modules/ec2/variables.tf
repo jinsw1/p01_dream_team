8582e3d076ea4df387642596323853a1a79486eb
gyu <leesk000107@naver.com>
2026-05-15 17:34:26 +0900

    구조 통합 promethues connect2

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -22,3 +22,15 @@ variable "root_volume_size" {
   type    = number
   default = 8
 }
+
+variable "tags" {
+  description = "어떤 EC2(bastion,was,db)인지 알기 위해 인스턴스에 추가할 태그"
+  type        = map(string)
+  default     = {}
+}
+
+variable "iam_instance_profile" {
+  description = "bastion 서버(for 프로메테우스)가 EC2 목록을 조회하기 위해 사용할 IAM 인스턴스 프로파일의 이름"
+  type        = string
+  default     = null
+}
